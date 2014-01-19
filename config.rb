83607151b3db4755c3a0e37c7d1f725e1bf6b4b8
Seigo Uchida <spesnova@gmail.com>
2014-01-19 20:01:16 +0900

    Configure blog prefix and permalink.

--- config.rb
+++ config.rb
@@ -6,11 +6,10 @@
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
-
-  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  #blog.prefix = "blog"
+  blog.permalink = "/blog/{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
-  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.sources = "/blogs/{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
   # blog.layout = "layout"
   # blog.summary_separator = /(READMORE)/
